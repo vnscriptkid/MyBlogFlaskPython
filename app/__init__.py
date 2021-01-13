060d1cce9eff185e9d73918975fb62085c34255a
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-13 22:16:41 +0700

    Integrate UI with bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from app import routes
 
