fd3d4c0843f8a17d4e9321b09fc0a4a47846ad70
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-14 23:15:41 +0700

    Set up unittest

--- flasky.py
+++ flasky.py
@@ -14,3 +14,11 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Role': Role}
 
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
