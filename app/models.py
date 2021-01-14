ef6eb90af1d81f70d8ae9b3423dc2aae92fc3802
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-14 23:41:03 +0700

    Password must be hashed
    Hash, verify and test it

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
@@ -10,6 +11,17 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
