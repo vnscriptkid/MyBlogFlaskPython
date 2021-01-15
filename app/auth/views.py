b042b353813728bda2e3adb29b955dc8dd01d7f2
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-15 22:01:54 +0700

    User can register
    Add register form, view function and UI

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from app.auth import auth
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
+
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -20,6 +22,20 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+
 @auth.route('/logout')
 @login_required
 def logout():
@@ -30,10 +46,5 @@ def logout():
 
 @auth.route('/reset', methods=['GET', 'POST'])
 def password_reset_request():
-    pass
-
-
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
     pass
 
