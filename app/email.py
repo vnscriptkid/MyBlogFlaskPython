a74ef98bb4d407622bd91f0f167730b1eb30bd19
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-16 02:17:19 +0700

    User must confirm by email for the first time

--- app/email.py
+++ app/email.py
@@ -1,15 +1,16 @@
 from threading import Thread
-from flask import render_template
+from flask import render_template, current_app
 from flask_mail import Message
 from app import mail
 
 
-def send_async_email(current_app, msg):
-    with current_app.app_context():
+def send_async_email(app, msg):
+    with app.app_context():
         mail.send(msg)
 
 
-def send_email(app, to, subject, template, **kwargs):
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
