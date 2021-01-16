4f70b562c6156ae6f387c732a9b7977950eb1205
Thanh Nguyen <vnscriptkid@gmail.com>
2021-01-16 15:55:38 +0700

    Admin can update user's info

--- app/main/views.py
+++ app/main/views.py
@@ -6,8 +6,8 @@ from flask_login import login_required, current_user
 from app import db
 from app.decorators import admin_required, permission_required
 from app.main import main
-from app.main.forms import EditProfileForm
-from app.models import Permission, User
+from app.main.forms import EditProfileForm, EditProfileAdminForm
+from app.models import Permission, User, Role
 
 
 @main.route('/')
@@ -57,6 +57,34 @@ def edit_profile():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)  # TODO: validate not found
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
 
 
 @main.route('/admin')
