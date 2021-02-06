764deb869d7eaa0976cfcbc447b33fc5b07c71d6
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-06 22:51:52 +0700

    Add API tests

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,7 +1,17 @@
-from flask import jsonify
+from flask import jsonify, request, render_template
 
 from app.api import api
 from app.exceptions import ValidationError
+from app.main import main
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    return render_template('404.html'), 404
 
 
 def bad_request(message):
--- app/models.py
+++ app/models.py
@@ -224,7 +224,7 @@ class Post(db.Model):
             'body': self.body,
             'body_html': self.body_html,
             'timestamp': self.timestamp,
-            'author_url': url_for('api.get_user', id=self.author_id),
+            'author_url': url_for('api.get_user', id=self.user_id),
             'comments_url': url_for('api.get_post_comments', id=self.id),
             'comment_count': self.comments.count()
         }
@@ -253,6 +253,24 @@ class Comment(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id),
+            'post_url': url_for('api.get_post', id=self.post_id),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author_url': url_for('api.get_user', id=self.user_id),
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
 
 
 db.event.listen(Comment.body, 'set', Comment.on_changed_body)
