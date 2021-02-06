225601334c811d7cefc30066a4ca26216f63621f
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-06 19:45:38 +0700

    Add support for test coverage

--- flasky.py
+++ flasky.py
@@ -1,10 +1,17 @@
 import os
+import sys
 
+import click
 from flask_migrate import Migrate
 
 from app import db, create_app
-from app.models import User, Post, Role
+from app.models import User, Post, Role, Permission, Comment
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -12,13 +19,37 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Role': Role}
+    return {'db': db, 'User': User, 'Post': Post, 'Role': Role, 'Permission': Permission, 'Comment': Comment}
 
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False, help='Run tests under code coverage.')
+@click.argument('test_names', nargs=-1)
+def test(coverage, test_names):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))
+
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+
+
 
