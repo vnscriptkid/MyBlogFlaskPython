02ff1c9709ab8c0092de7ca05245c233c02c87b6
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-07 14:36:38 +0700

    Measure performance with profile

--- flasky.py
+++ flasky.py
@@ -52,6 +52,12 @@ def test(coverage, test_names):
         COV.erase()
 
 
-if __name__ == '__main__':
-    app.run()
+@app.cli.command()
+@click.option('--length', default=25, help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None, help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length], profile_dir=profile_dir)
+    app.run(debug=False)
 
