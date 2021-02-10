c410eb04ca8bdf74ef3cb5c94d9d5a2ef4656a64
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-10 15:20:28 +0700

    Remove unnecessary line in edit_profile

--- app/main/views.py
+++ app/main/views.py
@@ -55,7 +55,6 @@ def edit_profile():
         current_user.name = form.name.data
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
-        db.session.add(current_user._get_current_object())
         db.session.commit()
         flash('Your profile has been updated.')
         return redirect(url_for('.user', username=current_user.username))
